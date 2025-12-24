ea13dfdd474dea01320502f4dd91a8b96e6c1609
coolsmarty786 <coolsmarty702@gmail.com>
2025-12-24 18:00:45 +0500

    Update and rename verify.html to verify.php

---
+++ verify.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<!-- Template Name: Clip-One - Responsive Admin Template build with Twitter Bootstrap 3.x Version: 1.4 Author: ClipTheme -->
+<!--[if IE 8]><html class="ie8 no-js" lang="en"><![endif]-->
+<!--[if IE 9]><html class="ie9 no-js" lang="en"><![endif]-->
+<!--[if !IE]><!-->
+<html lang="en" class="no-js">
+    <head>
+        <style>
+            .table_detail {
+                font-family: arial, sans-serif;
+                border-collapse: collapse;
+                width: 100%;
+                border: 3px solid #000;
+            }
+
+            .table_detail td, th {
+                border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+            }
+
+            .table_detail tr:nth-child(even) {
+                background-color: #dddddd;
+            }
+        </style>
+    </head>
+    <title>::SBCA::</title>
+    <body>
+        <div class="main-container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <!-- start: BASIC TABLE PANEL -->
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <center><img src="<?php echo base_url() . 'logo11.png'; ?>"></center>
+                                <br />
+                                    <table class="table_detail">
+                                        
+                                        
+                                        
+                                        <tr>
+                                            <td width="30%">File No</td>
+                                            <td width="70%"><?php echo 'Test'; ?></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>Acknowledgment Date</td>
+                                            
+                                            <td><?php echo 'Test'; ?></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>Town</td>
+                                            <td><?php echo 'Test'; ?></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>Owner Name</td>
+                                            <td>
+                                                <?php
+                                                    echo 'Test';
+                                                ?>
+                                            </td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>Licensed Professional Name</td>
+                                            <td><?php echo 'Test'; ?></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>Plot Area (in Sq. Yd)</td>
+                                            <td><?php echo 'Test'; ?></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>Status of Plot</td>
+                                            <td><?php echo 'Test'; ?></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>Proposed No. of Floors</td>
+                                            <td><?php echo 'Test'; ?></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>Covered Area in Sq. Ft.</td>
+                                            <td><?php echo 'Test'; ?></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>Type of Building</td>
+                                            <td><?php echo 'Test'; ?></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>Final Approval No.</td>
+                                            <td><?php echo 'Test'; ?></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>Final Approval Date</td>
+                                            <td><?php echo 'Test'; ?></td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Final Issued Date</td>
+                                            <td><?php echo 'Test' ?></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>Plot Number</td>
+                                            <td><?php echo 'Test'; ?></td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Proposal Status</td>
+                                            <td><?php echo 'Test'; ?></td>
+                                        </tr>
+                                        
+                                    </table>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        
+        
+        
+        <?php //echo "<pre>"; print_r($lp_data); exit; ?>
+
+    </body>
+</html>
